36586f2e11fc99fb2fc5efe41e471ecce244fced
brettmrice <brettmrice@gmail.com>
2022-04-08 22:55:54 -0400

    update index

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,6 +5,14 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
+  /*var y1 = document.getElementsByClassName("topnav-left")[0];
+  if (y1.className === "topnav-left") {
+    y1.className += " responsive";
+  }
+  var y2 = document.getElementsByClassName("topnav-centered")[0];
+  if (y2.className === "topnav-centered") {
+    y2.className += " responsive";
+  }*/
 }
 
 // Get the container element
@@ -18,6 +26,8 @@ for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    if(this.className !== "icon") {
+      this.className += " active";
+    }
   });
 }
